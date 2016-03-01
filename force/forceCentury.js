70a24463cc211519fd93bffc0516cd0c0466f59b
elektrobohemian <david@8bit-inferno.de>
2016-03-01 08:57:01 +0100

    feature complete
    * randomized drawing of markers pointing at one location
    * fixed an error that basically removed the contents of rows with a
    NULL/NaNed dateClean column
    * added manual corrections for spatial cluster names in order to
    retrieve them correctly with Google Maps

--- force/forceCentury.js
+++ force/forceCentury.js
@@ -12,8 +12,8 @@ var w = 1600, //3000,
     h = 1100 //3000,
 fill = d3.scale.category20();
 
-var overviewJSONpath = "century_test.json";
-//var overviewJSONpath = "century.json"
+//var overviewJSONpath = "century_test.json";
+var overviewJSONpath = "century.json"
 
 // flag showing if the user is currently inspecting a cluster
 var inClusterInspection = false;
@@ -68,7 +68,7 @@ document.body.addEventListener('keydown', function (e) {
     } else if (k == 68) { // 'd'
         if (queueData != null)
             displayDetailDialog(queueData, 0);
-    } else if (k == 69) { // 'e'
+    } else if (k == 77) { // 'm'
         locations = [];
         d3.selectAll("image").each(function (d) {
             if (d.lat != "nan") {
@@ -77,6 +77,7 @@ document.body.addEventListener('keydown', function (e) {
                 locArray.push(d.lat);
                 locArray.push(d.lng);
                 locArray.push(imgDir + d.imagePath + ".jpg");
+                locArray.push(d);
                 locations.push(locArray);
             }
         });
@@ -312,16 +313,49 @@ function initMap() {
     var marker, i;
 
     for (i = 0; i < locations.length; i++) {
+        // in case of multiple markers at one location, we place these markes randomized around the original location
+        latModifier = 1;
+        lngModifier = 1;
+        latOffset = Math.round(Math.random() * 10);
+        lngOffset = Math.round(Math.random() * 10);
+        if (latOffset % 2 == 0)
+            latModifier = 1;
+        else
+            latModifier = -1;
+        if (lngOffset % 2 == 0)
+            lngModifier = 1;
+        else
+            lngModifier = -1;
+        lat = parseFloat(locations[i][1]) + (Math.random() / 100.0) * latModifier;
+        lng = parseFloat(locations[i][2]) + (Math.random() / 100.0) * lngModifier;
 
         marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            position: new google.maps.LatLng(lat, lng),
             map: map
         });
 
         google.maps.event.addListener(marker, 'click', (function (marker, i) {
             return function () {
+                d = locations[i][4];
                 img = "<img height='150px' src='" + locations[i][3] + "' />";
-                infowindow.setContent(img + "<p>" + locations[i][0] + "</p>");
+
+                markerContent = "";
+                markerContent += "<p><b>" + cleanUp(d.title) + " (" + cleanUp(d.dateClean) + ")</b></p>";
+                markerContent += "<p>" + d.location + "</p>";
+                markerContent += "<p><span class='legend_creator'>&#9608;&nbsp;</span>Creator: " + cleanUp(d.creator) + "</p>";
+                markerContent += "<p><span class='legend_publisher'>&#9608;&nbsp;</span>Publisher: " + cleanUp(d.publisher) + "</p>";
+                markerContent += "<p><span class='legend_source'>&#9608;&nbsp;</span>Source: " + cleanUp(d.source) + "</p>";
+                markerContent += "<p><span class='legend_mediatype'>&#9608;&nbsp;</span>Mediatype: " + cleanUp(d.mediatype) + " (<span class='legend_subject'>&#9608;&nbsp;</span>" + cleanUp(d.subject) + ")</p>";
+
+                markerContent += "<ul>";
+                markerContent += "<li><a target='_blank' href='" + metsLink + d.name + "'>METS/MODS metadata</a></li>";
+
+                markerContent += "<li><a target='_blank' href='" + ppnLink + d.name.replace("PPN", "") + "'>Show in catalog</a></li>";
+                markerContent += "<li><a target='_blank' href='" + stabikatPlusLink + d.location + "'>Search for location in stabikat+ discovery system</a></li>";
+                markerContent += "</ul>";
+
+                infowindow.setContent(img + markerContent);
+                //infowindow.setContent(img + "<p>" + locations[i][0] + "</p>");
                 infowindow.open(map, marker);
             }
         })(marker, i));
